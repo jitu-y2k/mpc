7014d66df82cc357fcb06188d54c0488570453f1
hackathon <hackathon@visualstudio2>
2024-07-29 14:00:11 +0000

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var studentViewModel = new StudentViewModel()
+            {
+                Student = new Student() { Id = 1, Name = "Kunal" },
+                Standards = new List<Standard>()
+                {
+                    new Standard() {Id =1, Name= "6th" },
+                    new Standard() {Id =2, Name= "7th" }
+                }
+
+            };
+
+            //Ways to pass data to views
+            //1. ViewBag
+            //2. ViewData
+            //3. ViewModel - Model Binding
+            //4. TempData
+
+            //ViewBag.Student = student;
+            //ViewData["ApplicationName"] = student;
+
+            //TempData["Student"] = student;
+
+            //return RedirectToAction("Index", "Student");
+            return View(studentViewModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            //Student student = TempData["Student"] as Student;
+                        
+            return View("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        private static List<Student> studentList = new List<Student>() {
+            new Student(){Id =1, Name="Gitika"},
+            new Student(){Id =2, Name="Rajesh"},
+            new Student(){Id =3, Name="Rashmi"},
+        };
+        public StudentController()
+        {           
+        }
+        public ActionResult Index()
+        { 
+            return View(studentList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int id)
+        {
+            var student = studentList.FirstOrDefault(s=>s.Id==id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateStudent(Student student)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
